07e163627a6d4a8a266485c11d8ffc0c39d9f32b
Piyumath <68487811+piyumathh@users.noreply.github.com>
2024-02-22 11:27:33 -0500

    Generation
    -map generation works without a hitch
    - starting town design

---
+++ Source/MayorGame/MayorGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MayorGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MayorGame, "MayorGame" );
